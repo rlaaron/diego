ff9b489ba872800b068b38411883a769ff99094e
Aaron Rincon <rlaaron0305@gmail.com>
2023-11-09 01:56:38 -0600

    create edge Class

---
+++ src/Edge.java
@@ -0,0 +1,31 @@
+public class Edge {
+    private Node destination;
+    private int weight;
+
+    public Edge(Node destination, int weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setDestination(Node destination) {
+        this.destination = destination;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return String.valueOf(this.destination.getId());
+    }
+
+    
+}
