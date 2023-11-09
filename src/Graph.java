d55da8fe0db1979fc134f0f8eaee22cbd21ebfc8
Aaron Rincon <rlaaron0305@gmail.com>
2023-11-09 02:03:14 -0600

    create graph Class

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
-    
+    private Map<Node, List<Edge>> graph;
+
+    public Graph() {
+        this.graph = new HashMap<>();
+    }
+
+    public void addNode(Node node) {
+        graph.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(Node source, Node destination, int weight) {
+        if (!graph.containsKey(source) || !graph.containsKey(destination)) {
+            throw new IllegalArgumentException("Nodes must be in the graph before adding an edge.");
+        }
+
+        Edge edge = new Edge(destination, weight);
+        graph.get(source).add(edge);
+    }
+
+    public List<Edge> getEdges(Node node) {
+        return graph.get(node);
+    }
+
+    public List<Node> getNodes() {
+        List<Node> nodes = new ArrayList<>();
+        for (Node node : graph.keySet()) {
+            nodes.add(node);
+        }
+        return nodes;
+    }
+
+    public void printGraph() {
+        for (Node node : graph.keySet()) {
+            System.out.print(node.toString() + ": ");
+            for (Edge edge : graph.get(node)) {
+                System.out.print(edge.toString() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Node node : graph.keySet()) {
+            sb.append(node.toString() + ": ");
+            for (Edge edge : graph.get(node)) {
+                sb.append(edge.toString() + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
